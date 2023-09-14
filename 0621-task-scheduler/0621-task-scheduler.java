aad3809afdbda711c8901ec590de4d7ab2bc4f2f
Jorge G <jorgeg922@hotmail.com>
2023-09-14 14:01:08 -0700

    Time: 34 ms (24.15%), Space: 45 MB (20.11%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,14 +1,17 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b) -> b-a);
-        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
-        HashMap<Character,Integer> freq = new HashMap<>();
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();//store pair of -> task and time until it can be added to the pq for processing again
+        HashMap<Character,Integer> freq = new HashMap<>();//store freq of each task
+        
         for(char task : tasks){
             freq.put(task, freq.getOrDefault(task,0)+1);
         }
+        
         for(char task : freq.keySet()){
             pq.add(freq.get(task));
         }
+        
         int time = 0;
         
         while(!pq.isEmpty() || !q.isEmpty()){
