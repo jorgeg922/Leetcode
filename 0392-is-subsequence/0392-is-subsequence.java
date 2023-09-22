17f4257b2862317e64cd9818f70e54b3f74cbb32
Jorge G <jorgeg922@hotmail.com>
2023-09-22 12:17:52 -0700

    Time: 1 ms (89.69%), Space: 40 MB (95.27%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,23 +3,17 @@ class Solution {
         if(s.length() == 0){
             return true;
         }
-        if(t.length() < s.length()){
-            return false;
-        }
         
-        int needle = 0;
-        
-        for(int haystack =0; haystack < t.length(); haystack++){
-            char curr = t.charAt(haystack);
-            if(curr == s.charAt(needle)){
-                needle++;
-            }
-            
-            if(needle == s.length()){
-                return true;
+        int j = 0;
+        for(int i=0; i<t.length(); i++){
+            if(t.charAt(i) == s.charAt(j)){
+                j++;
+                if(j == s.length()){
+                    return true;
+                }
             }
         }
-        
+
         return false;
     }
 }
