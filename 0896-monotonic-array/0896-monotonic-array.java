36f5b0f651ad42b2fa30aa95183c7e8b77c53285
Jorge G <jorgeg922@hotmail.com>
2023-09-29 08:54:21 -0700

    Time: 1 ms (99.82%), Space: 55.9 MB (35.23%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -4,13 +4,13 @@ class Solution {
         int right = nums.length-1;
         
         if(nums[left] < nums[right]){
-            return verify(1,nums);
+            return verifyMonotonic(1,nums);
         }else{
-            return verify(-1,nums);
+            return verifyMonotonic(-1,nums);
         }
     }
     
-    public boolean verify(int direction, int[] nums){
+    public boolean verifyMonotonic(int direction, int[] nums){
         int prev = 0;
         for(int i = prev+1; i<nums.length; i++){
             switch(direction){
