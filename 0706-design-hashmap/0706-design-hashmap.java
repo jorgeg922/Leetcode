f1e5b516c88c90135a3c5aa23bdf8891068a31a0
Jorge G <jorgeg922@hotmail.com>
2023-10-04 10:41:04 -0700

    Time: 21 ms (48.39%), Space: 50.2 MB (47.99%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,71 @@
 class MyHashMap {
-    int[] map;
+    class Pair<U,V>{
+        U key;
+        V value;
+        public Pair(U key,V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    class Bucket{
+        List<Pair<Integer,Integer>> bucket;
+        public Bucket(){
+            bucket = new LinkedList<>();
+        }
+        
+        public int get(int key){
+            for(Pair<Integer,Integer> pair : bucket){
+                if(pair.key == key){
+                    return pair.value;
+                }
+            }
+            return -1;
+        }
+        
+        public void put(int key, int value){
+            for(Pair<Integer,Integer> pair : bucket){
+                if(pair.key == key){
+                    pair.value = value;
+                    return;
+                }
+            }
+            
+            bucket.add(new Pair<Integer,Integer>(key,value));
+        }
+        
+        public void remove(int key){
+            for(Pair<Integer,Integer> pair : bucket){
+                if(pair.key == key){
+                    bucket.remove(pair);
+                    return;
+                }
+            }
+        }
+    }
+    
+    int buckets = 2333;
+    List<Bucket> map;
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map,-1);
+        map = new ArrayList<>();
+        for(int i=0; i<buckets; i++){
+            map.add(new Bucket());
+        }
     }
     
     public void put(int key, int value) {
-        map[key]=value;
+        int location = key%buckets;
+        map.get(location).put(key, value);
     }
     
     public int get(int key) {
-        return map[key];
+        int location = key%buckets;
+        return map.get(location).get(key);
     }
     
     public void remove(int key) {
-        map[key]=-1;
+        int location = key%buckets;
+        map.get(location).remove(key);
     }
 }
 
