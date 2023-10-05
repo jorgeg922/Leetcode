9898ad783121da1a348ec3be9721cdc74864f388
Jorge G <jorgeg922@hotmail.com>
2023-10-04 20:32:47 -0700

    Time: 10 ms (49.32%), Space: 46.7 MB (38.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        int n = nums.length;
+        int ceiling = nums.length/3;
         HashMap<Integer,Integer> count = new HashMap<>();
         
         for(int num : nums){
             count.put(num,count.getOrDefault(num,0)+1);
-            if(count.get(num) > (n/3)+1){
-                continue;
-            }
-            if(count.get(num) > n/3){
-                ans.add(num);
-            }
         }
+        
+       for(Map.Entry<Integer,Integer> entry : count.entrySet()){
+           if(entry.getValue() > ceiling){
+               ans.add(entry.getKey());
+           }
+       }
         
         return ans;
     }
