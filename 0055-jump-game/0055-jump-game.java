0f4014b139adb42bd4a1f808943ff442160ea3be
Jorge G <jorgeg922@hotmail.com>
2023-10-07 19:54:38 -0700

    Time: 295 ms (11.45%), Space: 43.6 MB (95.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,41 @@
+enum Visited{
+    GOOD, BAD, UNK
+}
+
+class Solution {
+    int[] nums;
+    Visited[] memo;
+    public boolean canJump(int[] nums) {
+        this.memo = new Visited[nums.length];
+        Arrays.fill(memo, Visited.UNK);
+        this.nums = nums;
+        return dp(0);
+    }
+    
+    public boolean dp(int index){
+        if(index >= nums.length-1){
+            memo[index] = Visited.GOOD;
+            return true;
+        }
+        
+       if(memo[index] != Visited.UNK){
+            return memo[index]==Visited.GOOD ? true:false;
+        }
+        
+        if(nums[index] == 0){
+            return false;
+        }
+        
+        boolean reached = false;
+        
+        for(int i=1; i<=nums[index]; i++){
+           if(dp(index+i)){
+               memo[index] = Visited.GOOD;
+               return true;
+           }
+        }
+        
+        memo[index] = Visited.BAD;
+        return false;
+    }
+}
