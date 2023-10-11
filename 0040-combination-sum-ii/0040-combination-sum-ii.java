4e7a11cb322186df429622ce27c13d1893c4c9a3
Jorge G <jorgeg922@hotmail.com>
2023-10-11 09:59:09 -0700

    Time: 3 ms (72.78%), Space: 42.1 MB (76.82%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,19 +1,14 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
-    Set<List<Integer>> tmp = new HashSet<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        backtrack(candidates,target,0,new ArrayList<Integer>(),new HashSet<Integer>(),0);
-        
-        for(List<Integer> combination : tmp){
-            ans.add(combination);
-        }
+        backtrack(candidates,target,0,new ArrayList<Integer>(),0);
         return ans;
     }
     
-    public void backtrack(int[] candidates, int target, int currSum, ArrayList<Integer> currList, HashSet<Integer> set, int index){
+    public void backtrack(int[] candidates, int target, int currSum, ArrayList<Integer> currList, int index){
         if(currSum == target){
-            tmp.add(new ArrayList<>(currList));
+            ans.add(new ArrayList<>(currList));
             return;
         }
         
@@ -26,13 +21,12 @@ class Solution {
             if (i > index && candidates[i] == candidates[i - 1])
                 continue;
             if(currSum + candidates[i] <= target){
-                if(!set.contains(i)){
-                    set.add(i);
+                
                     currList.add(candidates[i]);
-                    backtrack(candidates, target, currSum+candidates[i],currList, set,i+1);
+                    backtrack(candidates, target, currSum+candidates[i],currList,i+1);
                     currList.remove(currList.size()-1);
-                    set.remove(i);
-                }                
+                   
+                               
             }    
         }
     }
