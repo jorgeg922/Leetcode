9917383421c27d752a67bf7eb4c5c6311c6165b0
Jorge G <jorgeg922@hotmail.com>
2023-10-15 20:29:49 -0700

    Time: 1 ms (100.00%), Space: 39.7 MB (51.61%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int arrangeCoins(int n) {
-        int blocks = 1;
-        int completed = 0;
-        while(n > 0 && n >= blocks){
-            n -= blocks;
-            completed++;
-            blocks++;
-        }
-        
-        return completed;
+  public int arrangeCoins(int n) {
+    long left = 0, right = n;
+    long k, curr;
+    while (left <= right) {
+      k = left + (right - left) / 2;
+      curr = k * (k + 1) / 2;
+
+      if (curr == n) return (int)k;
+
+      if (n < curr) {
+        right = k - 1;
+      } else {
+        left = k + 1;
+      }
     }
+    return (int)right;
+  }
 }
