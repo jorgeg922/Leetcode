74c5d03b780b5c007206b154d7d3a8b5de704e74
Jorge G <jorgeg922@hotmail.com>
2023-10-17 22:14:31 -0700

    Time: 1 ms (82.82%), Space: 41.2 MB (40.57%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,38 +1,20 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        if(str1.length() < str2.length()){
-            return gcdOfStrings(str2,str1);
-        }
-        
-        int rightBound = str2.length();
-        int largestStringLen = -1;
-        String largestString = "";
-        
-        for(int i=rightBound-1; i>=0; i--){
-            String sub = str2.substring(i,rightBound);
-            if(divide(str2, sub) && divide(str1, sub)){
-                if(sub.length() > largestStringLen){
-                    largestStringLen = sub.length();
-                    largestString = sub;
-                }
-            }
-        }
-        
-        return largestString;
+    public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }    
     }
     
-    public boolean divide(String s, String x){
-        int len = x.length();
-        int i=0;
-        for(i=0; i<=s.length()-len; i+=len){
-            String test = s.substring(i,i+len);
-            if(!test.equals(x)){
-                return false;
-            }
+    public String gcdOfStrings(String str1, String str2) {
+        // if strings added together are equals, then they share a GCD
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
-        if(i == s.length()){
-            return true;
-        }
-        return false;
+        
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
     }
 }
