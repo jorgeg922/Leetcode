23f1c54af7cb064ab3823a182306c001d0464497
Jorge G <jorgeg922@hotmail.com>
2023-10-27 09:17:05 -0700

    Time: 75 ms (75.74%), Space: 55.6 MB (9.95%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.java
@@ -0,0 +1,45 @@
+class FileSystem {
+    HashMap<String,Integer> existingPaths;
+    public FileSystem() {
+        existingPaths = new HashMap<>();
+    }
+    
+    public boolean createPath(String path, int value) {
+        if(path.isBlank() || path.equals("/")){
+            return false;
+        }
+        
+        if(existingPaths.containsKey(path)){
+            return false;
+        }
+        
+        int i = path.lastIndexOf("/");
+        String tmp = path;
+        while(i > 0){
+            tmp = path.substring(0,i);
+            if(!existingPaths.containsKey(tmp)){
+                return false;
+            }
+            
+            i = tmp.lastIndexOf("/");
+        }
+        
+        existingPaths.put(path,value);
+        return true;
+        
+    }
+    
+    public int get(String path) {
+        if(existingPaths.containsKey(path)){
+            return existingPaths.get(path);
+        }
+        return -1;
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
