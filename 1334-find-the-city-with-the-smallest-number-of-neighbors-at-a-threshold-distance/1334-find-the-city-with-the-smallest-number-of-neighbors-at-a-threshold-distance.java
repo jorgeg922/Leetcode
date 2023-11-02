c89887263b8081002200c89245f7f48268715507
Jorge G <jorgeg922@hotmail.com>
2023-11-02 16:36:30 -0700

    Time: 61 ms (13.63%), Space: 43.6 MB (35.43%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -46,7 +46,7 @@ class Solution {
             visited[city] = true;
             reached++;
             for(int col=0; col<grid[0].length;col++){
-                if(grid[city][col] > -1 ){
+                if(grid[city][col] > -1 && !visited[col]){
                     int remDistance = dist + grid[city][col];
                     if(remDistance <= distance){
                         pq.add(new int[]{col,remDistance});
