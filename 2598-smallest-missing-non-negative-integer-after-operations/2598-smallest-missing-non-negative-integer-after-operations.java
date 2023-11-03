f0031ea929db6e9fec24db04599a656c360133f1
Jorge G <jorgeg922@hotmail.com>
2023-11-03 12:15:12 -0700

    Time: 63 ms (31.30%), Space: 59 MB (23.66%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
@@ -1,28 +1,30 @@
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
         int len = nums.length;
-        HashMap<Integer,Integer> count = new HashMap<>();
+        Map<Integer,Integer> count = new HashMap<>();
         
-        //convert values to the mininum positive value, and log frequency
-        for(int i=0; i<len; i++){
-            int mod = nums[i] % value;
+        for(int num : nums){
+            int mod = num % value;
             if(mod < 0){
                 mod += value;
             }
-            count.put(mod,count.getOrDefault(mod,0)+1);
+            
+            count.put(mod, count.getOrDefault(mod,0)+1);
         }
         
-        //map above is keeping track of how many mins of a kind you can make
-        //now use the "index" to see if you could have made that number
-        for(int i=0; i<len; i++){
-            int check = i % value;
-            if(!count.containsKey(check) || count.get(check) == 0){
+        
+        for(int i=0;i<len;i++){
+            int smallInt = i%value;
+            if(!count.containsKey(smallInt)){
                 return i;
             }
             
-            count.put(check,count.get(check)-1);
+            count.put(smallInt,count.get(smallInt)-1);
+            if(count.get(smallInt)==0){
+                count.remove(smallInt);
+            }
         }
         
-        return nums.length;
+        return len;
     }
 }
