55174cc59773f4a924b98dc9d32971e4b2adb1e8
Jorge G <jorgeg922@hotmail.com>
2023-11-05 11:31:50 -0800

    Time: 11 ms (90.93%), Space: 43.7 MB (66.19%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -19,7 +19,7 @@ class Solution {
     public int dp(int r1, int c1, int c2){
         int r2 = r1 + c1 - c2;
         if(N == r1 || N == r2 || N == c1 || N == c2 || grid[r1][c1] == -1 || grid[r2][c2] == -1){
-            return -999999;
+            return Integer.MIN_VALUE;
         }else if(r1 == N-1 && c1 == N-1){
             return grid[r1][c1];
         }else if(memo[r1][c1][c2] != Integer.MIN_VALUE){
