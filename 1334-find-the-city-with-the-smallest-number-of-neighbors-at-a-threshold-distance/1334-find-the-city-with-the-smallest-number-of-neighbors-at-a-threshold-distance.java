1ca472afc7bca5a45b88e81313aa5500af736c50
Jorge G <jorgeg922@hotmail.com>
2023-11-07 21:37:57 -0800

    Time: 78 ms (9.77%), Space: 43.8 MB (23.52%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,7 +1,11 @@
 class Solution {
+    int distanceThreshold;
+    int N;
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] grid = new int[n][n];
-        for(int[] row : grid){
+        this.distanceThreshold = distanceThreshold;
+        this.N = n;
+        int[][] graph = new int[n][n];
+        for(int[] row : graph){
             Arrays.fill(row,-1);
         }
         for(int[] edge : edges){
@@ -9,52 +13,56 @@ class Solution {
             int y = edge[1];
             int w = edge[2];
             
-            grid[x][y]=w;
-            grid[y][x]=w;
+            graph[x][y]=w;
+            graph[y][x]=w;
         }
         
-        int minCitiesReached = Integer.MAX_VALUE;
         int city = -1;
-        
+        int minReachable = Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
-            int citiesReached = findReachableCities(i,grid,distanceThreshold);
-            if(minCitiesReached >= citiesReached){
-                minCitiesReached = citiesReached;
+            int citiesReached = dfs(i,graph);
+            if(minReachable >= citiesReached){
+                minReachable = citiesReached;
                 city = i;
             }
-            
         }
-                                        
+        
         return city;
     }
-                                        
-    public int findReachableCities(int node, int[][] grid, int distance){
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        int[] curr = {node,0};
-        pq.add(curr);
+    
+    public int dfs(int city, int[][] graph){
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(int[] a, int[] b){
+                return a[1]-b[1];
+            }
+        });
         
-        boolean[] visited = new boolean[grid.length];
-        int reached = 0;
+        Set<Integer> visited = new HashSet<>();
+        pq.add(new int[]{city,0});
+        int citiesReached = 0;
         
-        while(!pq.isEmpty()){
-            int[] currNode = pq.poll();
-            int city = currNode[0];
-            int dist = currNode[1];
-            if(visited[city]){
+        while(!pq.isEmpty()){          
+            int[] edge = pq.poll();
+            int newcity = edge[0];            
+            int dist = edge[1];
+            
+            if(visited.contains(newcity)){
                 continue;
             }
-            visited[city] = true;
-            reached++;
-            for(int col=0; col<grid[0].length;col++){
-                if(grid[city][col] > -1 && !visited[col]){
-                    int remDistance = dist + grid[city][col];
-                    if(remDistance <= distance){
-                        pq.add(new int[]{col,remDistance});
-                    }
+            
+            citiesReached++;
+            visited.add(newcity);
+            for(int col=0; col<N; col++){
+                if(graph[newcity][col] != -1 && !visited.contains(col)){
+                    int distanceTraveled =  dist+graph[newcity][col];
+                    if(distanceTraveled <= distanceThreshold){
+                         pq.add(new int[]{col,dist+graph[newcity][col]});
+                    }                   
                 }
             }
         }
         
-        return reached;
+        return citiesReached;
+        
     }
 }
