68817d770a7c77dea29cc58e03c283d781e2f3a1
Jorge Gonzales <jorgeg922@hotmail.com>
2023-11-20 08:49:59 -0800

    Time: 24 ms (63.07%), Space: 65.8 MB (29.36%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int paperTruckLocation =0;
+        int paperTruckTravelTime = 0;
+        int metalTruckLocation =0;
+        int metalTruckTravelTime =0;
+        int glassTruckLocation =0;
+        int glassTruckTravelTime =0;
+        
+        int[] travelTimes = new int[garbage.length];
+        travelTimes[0] = 0;
+        for(int i =1; i<garbage.length; i++){
+            travelTimes[i] = travelTimes[i-1] + travel[i-1];
+        }
+        
+        for(int i=0; i<garbage.length; i++){
+            char[] bins = garbage[i].toCharArray();
+            
+            for(char trashType : bins){
+                switch(trashType){
+                    case 'G':
+                        glassTruckTravelTime += travelTimes[i] - travelTimes[glassTruckLocation] + 1;
+                        glassTruckLocation = i;
+                        break;
+                    case 'P':
+                        paperTruckTravelTime += travelTimes[i] - travelTimes[paperTruckLocation] + 1;
+                        paperTruckLocation = i;
+                        break;
+                    case 'M':
+                        metalTruckTravelTime += travelTimes[i] - travelTimes[metalTruckLocation] + 1;
+                        metalTruckLocation = i;
+                        break;
+                }
+            }
+        }
+        return paperTruckTravelTime + metalTruckTravelTime + glassTruckTravelTime;
+    }
+}
