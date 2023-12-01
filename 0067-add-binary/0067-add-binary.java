4ccbc7d94346c59983a321a45f09081c7241db7c
Jorge Gonzales <jorgeg922@hotmail.com>
2023-12-01 08:49:51 -0800

    Time: 1 ms (100.00%), Space: 41.3 MB (48.30%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+        
+        if(n<m){
+            return addBinary(b,a);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int j = m-1;
+        for(int i=n-1; i>=0; i--){
+            if(a.charAt(i) == '1'){
+                carry++;
+            }
+            if(j>=0 && b.charAt(j--) == '1'){
+                carry++;
+            }
+            
+            if(carry%2==1){
+                sb.append('1');
+            }else{
+                sb.append('0');
+            }
+            
+            carry /= 2;
+        }
+        
+        if(carry == 1){
+            sb.append('1');
+        }
+        
+        return sb.reverse().toString();
+    }
+}
