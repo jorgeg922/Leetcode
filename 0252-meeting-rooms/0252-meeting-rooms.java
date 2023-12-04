7e3152873b5f72e49a41de730f35d16425f87e2e
Jorge Gonzales <jorgeg922@hotmail.com>
2023-12-04 10:37:09 -0800

    Time: 6 ms (84.59%), Space: 44.9 MB (9.59%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -4,15 +4,7 @@ class Solution {
             return true;
         }
         
-        Arrays.sort(intervals, new Comparator<>(){
-            public int compare(int[] a, int[] b){
-                if(a[0] == b[0]){
-                    return a[1]-b[1];         
-                }
-                
-                return a[0] - b[0];
-            }
-        });
+        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
         
         for(int i=1; i<intervals.length; i++){
             int start = intervals[i][0];           
