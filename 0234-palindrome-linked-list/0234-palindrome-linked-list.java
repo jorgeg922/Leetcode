b260ad02a2461e3cb83cd93b8504c0c019f9b91d
Jorge Gonzales <jorgeg922@hotmail.com>
2023-12-05 15:46:53 -0800

    Time: 6 ms (46.09%), Space: 55.9 MB (88.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,27 +11,54 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-        StringBuilder sb = new StringBuilder();
-        ListNode curr = head;
-        
-        while(curr != null){
-            sb.append(Integer.toString(curr.val));
-            curr = curr.next;
+        if(head == null){
+            return true;
         }
         
-        int left = 0;
-        int right = sb.length()-1;
+        ListNode endFirstHalf = getEndFirstHalf(head);
+        ListNode startSecondHalf = reverse(endFirstHalf.next);
         
-        while(left < right){
-            if(sb.charAt(left) != sb.charAt(right)){
-                return false;
+        ListNode p1 = head;
+        ListNode p2 = startSecondHalf;
+        boolean res = true;
+        
+        while(res && p2 != null){
+            if(p1.val != p2.val){
+                res = false;
             }
             
-            left++;
-            right--;
+            p1 = p1.next;
+            p2 = p2.next;
         }
         
-        return true;
+        reverse(startSecondHalf);
         
+        return res;
+    }
+    
+    public ListNode reverse(ListNode node){
+        ListNode prev = null;
+        ListNode curr = node;
+        
+        while(curr != null){
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        
+        return prev;
+        
+    }
+    public ListNode getEndFirstHalf(ListNode node){
+        ListNode slow = node;
+        ListNode fast = node;
+        
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
     }
 }
