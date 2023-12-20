140c6d6f0eb725d2e72633557540d76328178416
Jorge Gonzales <jorgeg922@hotmail.com>
2023-12-20 15:40:12 -0800

    Time: 4 ms (44.30%), Space: 49.4 MB (26.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,21 +4,23 @@ class Solution {
         int cols = grid[0].length;
         
         boolean[][] visited = new boolean[rows][cols];
-        int numberOfIslands = 0;
+        
+        int islands = 0;
+        
         for(int row=0; row<rows; row++){
             for(int col=0; col<cols; col++){
                 if(grid[row][col]=='1' && !visited[row][col]){
                     dfs(row,col,grid,visited);
-                    numberOfIslands++;
+                    islands++;
                 }
             }
         }
         
-        return numberOfIslands;
+        return islands;
     }
     
     public void dfs(int row, int col, char[][] grid, boolean[][] visited){
-        if(row<0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col] == '0'){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col]=='0'){
             return;
         }
         
