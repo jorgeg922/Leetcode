7a522fa671f9a644edde6b5711242789a3cec87f
Jorge Gonzales <jorgeg922@hotmail.com>
2024-01-14 09:17:00 -0800

    Time: 48 ms (23.57%), Space: 60.2 MB (6.89%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -0,0 +1,55 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if(edges.length == 0){           
+            return Arrays.asList(0);
+        }
+        if(n < 2){
+             return Arrays.asList(0,1);
+        }
+        HashMap<Integer,Set<Integer>> graph = new HashMap<>();
+        
+        for(int[] edge : edges){
+            int a = edge[0];
+            int b = edge[1];
+            
+            if(!graph.containsKey(a)){
+                graph.put(a, new HashSet<>());
+            }
+            
+            if(!graph.containsKey(b)){
+                graph.put(b, new HashSet<>());
+            }
+            
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        Queue<Integer> leafs = new LinkedList<>();
+        for(Map.Entry<Integer,Set<Integer>> entry : graph.entrySet()){
+            if(entry.getValue().size() == 1){
+                leafs.add(entry.getKey());
+            }
+        }
+        
+        int remainingNodes = n-1;
+        while(remainingNodes > 1){
+            int size = leafs.size();
+            remainingNodes -= size;
+            for(int i=0; i<size; i++){
+                int currentLeaf = leafs.poll();
+                int neighbor = graph.get(currentLeaf).iterator().next();
+                graph.get(neighbor).remove(currentLeaf);
+                if(graph.get(neighbor).size() == 1){
+                    leafs.add(neighbor);
+                }
+            }
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        while(!leafs.isEmpty()){
+            ans.add(leafs.poll());
+        }
+        
+        return ans;
+    }
+}
