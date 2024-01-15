2dc999e129f583b3a5a77e9ab94374c8bbc651c8
Jorge Gonzales <jorgeg922@hotmail.com>
2024-01-15 09:03:37 -0800

    Time: 0 ms (100.00%), Space: 41.3 MB (6.08%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,31 @@
+class Solution {
+    int[] nums;
+    int size;
+    public int rob(int[] nums) {
+        this.nums = nums;
+        size = nums.length;
+        int[] mem = new int[size];
+        Arrays.fill(mem,-1);
+        
+        return dp(0, mem);
+    }
+    
+    public int dp(int i, int[] mem){
+        if(i == nums.length-1){
+            return nums[i];
+        }
+        
+        if(i == nums.length-2){
+            return Math.max(nums[i],nums[i+1]);
+        }
+        
+        if(mem[i] != -1){
+            return mem[i];
+        }
+        
+        int maxProfit = Math.max(dp(i+1,mem),dp(i+2,mem)+nums[i]);
+        
+        mem[i] = maxProfit;
+        return maxProfit;
+    }
+}
