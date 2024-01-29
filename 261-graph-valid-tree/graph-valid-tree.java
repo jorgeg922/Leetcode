ab9eaae4d18836e5a9f8f1d3ce7e861201841e8a
Jorge Gonzales <jorgeg922@hotmail.com>
2024-01-29 12:50:28 -0800

    Time: 10 ms (8.84%) | Memory: 45.9 MB (5.87%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(n == 1 && edges.length==0){
+            return true;
+        }
+        HashMap<Integer,List<Integer>> graph = new HashMap<>();
+        for(int[] edge : edges){
+            int x = edge[0];
+            int y = edge[1];
+            
+            if(!graph.containsKey(x)){
+                graph.put(x, new ArrayList<>());
+            }
+            
+            if(!graph.containsKey(y)){
+                graph.put(y, new ArrayList<>());
+            }
+            
+            graph.get(x).add(y);
+            graph.get(y).add(x);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> inQueue = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
+        
+        for(int i=0; i<n; i++){
+            if(!graph.containsKey(i)){
+                return false;
+            }
+        }
+        
+        q.add(0);
+        inQueue.add(0);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int curr = q.poll();
+                inQueue.remove(curr);
+                int repLink = 0;
+                seen.add(curr);
+                for(int link : graph.get(curr)){
+                    if(!seen.contains(link)){
+                        if(inQueue.contains(link)){
+                            return false;
+                        }
+                        q.add(link);
+                        inQueue.add(link);
+                    }
+                }
+            }
+        }
+        
+        if(seen.size() != n){
+            return false;
+        }
+        
+        return true;
+    }
+}
