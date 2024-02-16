2ec33814aa7efa8519441908e4fb458c00e7c864
Jorge Gonzales <jorgeg922@hotmail.com>
2024-02-16 02:09:39 -0800

    Time: 5 ms (9.47%) | Memory: 44.3 MB (14.62%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         Stack<Pair<Integer,Integer>> stack = new Stack<>();
-        int[] finalPrices = new int[prices.length];
-        int ptr = 0;
+        int[] finalPrices = new int[prices.length];        
         
         for(int i=0; i<prices.length; i++){
-            if(stack.isEmpty() || stack.peek().getValue() < prices[i]){
-                stack.push(new Pair(i,prices[i]));
-            }else{
+            
                 while(!stack.isEmpty() && stack.peek().getValue() >= prices[i]){
                     Pair<Integer,Integer> curr = stack.pop();
                     finalPrices[curr.getKey()] = curr.getValue() - prices[i];
                 }
                 stack.push(new Pair(i,prices[i]));
-            }
+            
         }
         
         while(!stack.isEmpty()){
