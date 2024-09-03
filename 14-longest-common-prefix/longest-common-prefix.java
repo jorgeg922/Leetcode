dada5a176f6dc3762e13066baa495627b792d262
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-02 22:46:57 -0700

    Time: 4 ms (18.65%) | Memory: 41.9 MB (23.25%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,51 @@
+class Solution {
+    class Trie{
+        Map<Character,Trie> links = new HashMap<>();
+        boolean isEndOfWord = false;
+        
+        public void insert(String word){
+            Trie curr = this;
+            for(char c : word.toCharArray()){
+                if(curr.links.containsKey(c)){
+                    curr = curr.links.get(c);
+                    continue;
+                }
+                curr.links.put(c, new Trie());
+                curr = curr.links.get(c);
+            }
+            curr.isEndOfWord = true;
+        }
+        
+        public String searchForLongestPrefix(){
+            StringBuilder sb = new StringBuilder();
+            if(links.size() > 1){
+                return sb.toString();
+            }
+            Trie curr = this;
+            
+            while(!curr.isEndOfWord){
+                if(curr.links.size() > 1){
+                    return sb.toString();
+                }
+                for(Map.Entry<Character,Trie> link : curr.links.entrySet()){
+                    sb.append(link.getKey());
+                    if(curr.isEndOfWord){
+                        return sb.toString();
+                    }
+                    curr = link.getValue();
+                }
+            }
+            
+            return sb.toString();
+        }
+        
+    }
+    public String longestCommonPrefix(String[] strs) {
+        Trie trie = new Trie();
+        for(String str : strs){
+            trie.insert(str);
+        }
+        
+        return trie.searchForLongestPrefix();
+    }
+}
