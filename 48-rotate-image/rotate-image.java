7014977c4b67d38b1ba361f84c023d79f68e9afc
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-07 15:44:19 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (80.21%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,26 +1,31 @@
 class Solution {
-    public void rotate(int[][] matrix) { 
-        int[][] newMatrix = new int[matrix.length][matrix[0].length];
-        for(int row=0; row<matrix.length; row++){        
-            for(int col = row; col < matrix[row].length; col++){
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        for(int row=0; row<n; row++){
+            for(int col=0; col<n; col++){
+                int ro = n-1-col;
+                int co = n-1-row;
+                
+                if(row==ro && col==co){
+                    break;
+                }
+                
                 int tmp = matrix[row][col];
-                matrix[row][col] = matrix[col][row];
-                matrix[col][row] = tmp;
+                matrix[row][col] = matrix[ro][co];
+                matrix[ro][co]=tmp;
             }
         }
         
-        for(int row=0; row<matrix.length; row++){
-            int left = 0;
-            int right = matrix[0].length-1;
-            
-            while(left < right){
-                int tmp = matrix[row][left];
-                matrix[row][left] = matrix[row][right];
-                matrix[row][right] = tmp;
-                
-                left++;
-                right--;
-            }
+        int left=0;
+        int right=n-1;
+        
+        while(left < right){
+            int[] tmp = matrix[left];
+            matrix[left] = matrix[right];
+            matrix[right]=tmp;
+            left++;
+            right--;
         }
     }
 }
