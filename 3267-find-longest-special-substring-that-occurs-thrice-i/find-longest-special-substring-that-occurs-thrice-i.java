2c2f8d51fb064f9c83b7ea19bea6af695e20267c
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-14 00:19:49 -0700

    Time: 14 ms (31.85%) | Memory: 45.3 MB (18.53%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maximumLength(String s) {
+       HashMap<String, Integer> freq = new HashMap<>();
+        
+        
+        for(int left=0; left<s.length(); left++){
+            String sub = s.substring(left, left+1);
+            freq.put(sub, freq.getOrDefault(sub,0)+1);
+            
+            int right = left+1;
+            StringBuilder sb = new StringBuilder();
+            sb.append(sub);
+            while(right < s.length()){
+                if(sb.charAt(sb.length()-1) == s.charAt(right)){
+                    sb.append(s.charAt(right));
+                    freq.put(sb.toString(), freq.getOrDefault(sb.toString(),0)+1);
+                    
+                }else{
+                    break;
+                }
+                right++;
+            }
+            
+
+        }
+        
+        int len = 0;
+        int count = 0;
+        for(Map.Entry<String,Integer> entry : freq.entrySet()){
+            if(entry.getValue() >= 3){
+                if(entry.getKey().length() > len){
+                    len = entry.getKey().length();
+                    count = entry.getValue();
+                }
+            }
+        }
+        
+        return len==0?-1:len;
+    }
+}
