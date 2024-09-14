edda84861defc957b605db84fb01794f7fccff61
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-14 13:37:00 -0700

    Time: 367 ms (14.87%) | Memory: 45 MB (25.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,25 @@
 class Solution {
     HashMap<Integer,Integer> memo = new HashMap<>();
+    
     public int lengthOfLIS(int[] nums) {
         int longest = 0;
-        for(int i=0; i<nums.length; i++){
-            longest = Math.max(longest, dp(i,nums));
+        for (int i = 0; i < nums.length; i++) {
+            longest = Math.max(longest, dp(i, nums));
         }
         return longest;
     }
 
-    public int dp(int index, int[] nums){
-        if(memo.containsKey(index)){
+    public int dp(int index, int[] nums) {
+        if (memo.containsKey(index)) {
             return memo.get(index);
         }
 
         int len = 1;
 
-        for(int i=0; i<index; i++){
-            if(nums[index] > nums[i]){
-                len = Math.max(len, dp(i,nums) + 1);
+        // Look forward to find the subsequences
+        for (int i = index + 1; i < nums.length; i++) {
+            if (nums[i] > nums[index]) {
+                len = Math.max(len, dp(i, nums) + 1);
             }
         }
 
