72250b88a0d27854589204c0d49d6e00208602db
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-16 00:03:33 -0700

    Time: 27 ms (27.03%) | Memory: 46.3 MB (59.39%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        int n = 0;
+        char operation = '+';
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(Character.isDigit(c)){
+                currentNumber = (currentNumber * 10) + (c - '0');
+                n+=1;
+            }else{  
+                n=0;
+            }
+            
+            if((!Character.isDigit(c) && !Character.isWhitespace(c)) || i == s.length()-1){
+                    switch(operation){
+                        case '-':
+                            stack.push(-currentNumber);
+                            break;
+                        case '+':
+                            stack.push(currentNumber);
+                            break;
+                        case '/':
+                            stack.push(stack.pop()/currentNumber);
+                            break;
+                        case '*':
+                            stack.push(stack.pop()*currentNumber);
+                            break;
+                    }
+            operation = c;
+            currentNumber = 0;
+            }
+                
+          }
+        
+        
+        int result = 0;
+        while(!stack.isEmpty()){
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
