8aa1125a9cd34ba174d2100598facb2f51264c08
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-09-21 23:09:08 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (38.42%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,45 +1,41 @@
 class Solution {
-    int seen[][];
-    int[][] grid;
+    int[][] obstacleGrid;
     int rows;
     int cols;
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {        
-        grid = obstacleGrid;
-        rows = obstacleGrid.length;
-        cols = obstacleGrid[0].length;
-        seen = new int[rows][cols];
-        
-         if(obstacleGrid[rows-1][cols-1] == 1){
+    int[][] visited;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        this.obstacleGrid = obstacleGrid;
+        this.rows = obstacleGrid.length;
+        this.cols = obstacleGrid[0].length;
+        this.visited = new int[rows][cols];
+        for(int[] entry : visited){
+            Arrays.fill(entry,-1);
+        }
+        if(obstacleGrid[rows-1][cols-1] == 1){
             return 0;
         }
-        
-        for(int entry[] : seen){
-            Arrays.fill(entry, -1);
-        }
-               
-        return genPaths(rows-1,cols-1);
+        return dfs(rows-1,cols-1);
     }
     
-    public int genPaths(int row, int col){
+    public int dfs(int row, int col){
         if(row == 0 && col == 0){
             return 1;
         }
         
-        if(seen[row][col] != -1){
-            return seen[row][col];
+        if(visited[row][col] != -1){
+            return visited[row][col];
         }
         
-        int paths = 0;
-        
-        if(row - 1 >= 0 && grid[row-1][col] != 1){
-            paths += genPaths(row-1, col);
+        int uniquePaths = 0;
+        if(row-1 >= 0 && obstacleGrid[row-1][col] != 1){
+            uniquePaths += dfs(row-1,col);
         }
         
-        if(col - 1 >= 0 && grid[row][col-1] != 1){
-            paths += genPaths(row, col-1);
+        if(col-1 >=0 && obstacleGrid[row][col-1] != 1){
+            uniquePaths += dfs(row, col-1);
         }
         
-        seen[row][col] = paths;
-        return paths;
+        visited[row][col] = uniquePaths;
+        return uniquePaths;
     }
 }
