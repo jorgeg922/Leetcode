a9e380f018c471917d1b7f9b79375ab56e9acdfa
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-11-14 08:24:42 -0800

    Time: 4 ms (51.21%) | Memory: 48.5 MB (97.25%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,36 @@
 class Solution {
-    int[][] locations = {{1,0},{-1,0},{0,1},{0,-1}};
-    char[][] grid;
-    int rows;
-    int cols;
+    int[][] neighbors = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
     public int numIslands(char[][] grid) {
-        int num_islands = 0;
-        this.grid = grid;
-        rows = grid.length;
-        cols = grid[0].length;
+        int ans = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
         boolean[][] visited = new boolean[rows][cols];
         
         for(int row=0; row<rows; row++){
             for(int col=0; col<cols; col++){
                 if(grid[row][col] == '1' && !visited[row][col]){
-                    num_islands++;
-                    dfs(row, col, visited);
-                }
+                    dfs(row,col, grid, visited);
+                    ans++;
+                }        
             }
         }
         
-        return num_islands;
-        
+        return ans;
     }
     
-    public void dfs(int row, int col, boolean[][] visited){
-        if(row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || grid[row][col] == '0'){
+    public void dfs(int row, int col, char[][] grid, boolean[][] visited){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col] == '0'){
             return;
         }
         
         visited[row][col] = true;
-        for(int[] location : locations){
-            int nextRow = row + location[0];
-            int nextCol = col + location[1];
-            dfs(nextRow, nextCol, visited);
+        
+        for(int[] neighbor : neighbors){
+            int nextRow = row + neighbor[0];
+            int nextCol = col + neighbor[1];
+            
+            dfs(nextRow, nextCol, grid, visited);
         }
     }
 }
