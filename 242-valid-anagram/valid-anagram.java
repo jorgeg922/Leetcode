c41404278d4e1845454450cd3a7d3b2768480cc7
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-11-20 20:23:03 -0800

    Time: 16 ms (24.20%) | Memory: 44.3 MB (57.79%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> freq = new HashMap<>();
+        
+        for(char c : s.toCharArray()){
+            freq.put(c, freq.getOrDefault(c,0)+1);
+        }
+        
+        for(char c : t.toCharArray()){
+            if(!freq.containsKey(c)){
+                return false;
+            }
+            
+            freq.put(c, freq.get(c)-1);
+            if(freq.get(c) == 0){
+                freq.remove(c);
+            }
+        }
+        
+        return freq.size()==0?true:false;
+    }
+}
