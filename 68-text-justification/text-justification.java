347ba80df3247960f09e8ae024a0bcd1af43ff89
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-11-24 00:26:49 -0800

    Time: 2 ms (42.80%) | Memory: 41.8 MB (48.69%) - LeetSync

--- 68-text-justification/text-justification.java
+++ 68-text-justification/text-justification.java
@@ -1,96 +1,100 @@
 class Solution {
-    int maxWidth;
     public List<String> fullJustify(String[] words, int maxWidth) {
-        this.maxWidth = maxWidth;
         List<String> ans = new ArrayList<>();
+        
+        List<String> line = new ArrayList<>();
+        int lineLen = 0;
         StringBuilder sb = new StringBuilder();
         
         for(String word : words){
+            int wordLen = word.length();
             sb.append(word);
             sb.append(" ");
-            if(sb.length()-1 > maxWidth){
-                String justified = justify(sb.toString().substring(0, sb.length()-word.length()-2)); 
-                //System.out.println(justified.length());
-                ans.add(justified);
-                sb = new StringBuilder();
-                sb.append(word);
+            if(sb.length() - 1 > maxWidth){
+                String currLine = sb.toString().substring(0, sb.length()-wordLen-2);               
+                String justified = justify(currLine, maxWidth);
+                ans.add(justified);                
+                sb = new StringBuilder(word);
                 sb.append(" ");
             }
-            
         }
         
         if(sb.length() > 0){
-            ans.add(leftJustify(sb.toString().trim()));
+            String leftJustified = leftJustify(sb.toString().trim(), maxWidth);
+            ans.add(leftJustified);
         }
-        
         
         return ans;
     }
     
-    private String leftJustify(String s){
-        int numberOfCharacters = s.length();       
-        int numberOfSpaces = maxWidth - numberOfCharacters;
-        StringBuilder sb = new StringBuilder(s);
-        for(int i=0; i<numberOfSpaces; i++){
+    public String leftJustify(String line, int maxWidth){
+        int spacesNeeded = maxWidth - line.length();
+        
+        StringBuilder sb = new StringBuilder(line);
+        for(int i=0; i<spacesNeeded; i++){
             sb.append(" ");
         }
+        
         return sb.toString();
     }
-    private String justify(String s){
-        String[] words = s.split(" ");
-        
-        int numberOfCharacters = (s.length() - (words.length-1));       
-        int numberOfSpaces = maxWidth - numberOfCharacters;
+    
+    public String justify(String line, int maxWidth){
+        String[] words = line.split(" ");
+        int numberOfCharacters = line.length() - (words.length-1);
+        int numberOfSpaces = maxWidth-numberOfCharacters;
         
         int spacesBetweenWords = 0;
         if(words.length > 1){
-             spacesBetweenWords = numberOfSpaces/(words.length-1);
+          spacesBetweenWords = numberOfSpaces/(words.length-1);  
         }
-       
+        
         int oddSpaces = 0;
         if(words.length > 1){
-            oddSpaces = numberOfSpaces%(words.length-1);
+            oddSpaces = numberOfSpaces % (words.length-1);
         }else{
             oddSpaces = numberOfSpaces;
         }
         
-        String spaces = "";
-        for(int j=0; j<spacesBetweenWords; j++){
-            spaces+=" ";
-        }
-        
-        List<String> justifiedWordArray = new ArrayList<>();
+        List<String> lineWithSpaces = new ArrayList<>();
         List<Integer> spaceIndexes = new ArrayList<>();
+        
+        String space = "";
+        for(int i=0; i<spacesBetweenWords; i++){
+            space += " ";
+        }
+        
         for(int i=0; i<words.length; i++){
-            justifiedWordArray.add(words[i]);
+            lineWithSpaces.add(words[i]);
             if(i < words.length-1){
-                justifiedWordArray.add(spaces);
-                spaceIndexes.add(justifiedWordArray.size()-1);
-            }            
-        }
-        
-        if(justifiedWordArray.size() == 1){
-            justifiedWordArray.add("");
-            spaceIndexes.add(justifiedWordArray.size()-1);
-        }
-        
-        int spacesPtr = 0;
-        while(oddSpaces > 0){
-            if(spacesPtr >= spaceIndexes.size()){
-                spacesPtr = 0;
+                lineWithSpaces.add(space);
+                spaceIndexes.add(lineWithSpaces.size()-1);
             }
-            String currentSpaces = justifiedWordArray.get(spaceIndexes.get(spacesPtr));
-            currentSpaces += " ";
-            justifiedWordArray.set(spaceIndexes.get(spacesPtr), currentSpaces);
-            spacesPtr++;
+        }
+        
+        if(lineWithSpaces.size() == 1){
+            lineWithSpaces.add("");
+            spaceIndexes.add(lineWithSpaces.size()-1);
+        }
+        
+        int ptr = 0;
+        while(oddSpaces > 0){
+            if(ptr >= spaceIndexes.size()){
+                ptr = 0;
+            }
+            String currSpace = lineWithSpaces.get(spaceIndexes.get(ptr));
+            currSpace += " ";
+            lineWithSpaces.set(spaceIndexes.get(ptr), currSpace);
             oddSpaces--;
+            ptr++;
         }
         
         StringBuilder sb = new StringBuilder();
-        for(String word : justifiedWordArray){
-            sb.append(word);
+        
+        for(String val : lineWithSpaces){
+            sb.append(val);
         }
         
         return sb.toString();
+        
     }
 }
