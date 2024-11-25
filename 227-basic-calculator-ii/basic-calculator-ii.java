dfa7df6ef34156615ce75c408f5732dc48a367c4
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-11-25 14:27:25 -0800

    Time: 27 ms (27.48%) | Memory: 46.2 MB (53.87%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,45 +1,40 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-        int currentNumber = 0;
-        int n = 0;
         char operation = '+';
-        
+        int currentNumber = 0;
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             
             if(Character.isDigit(c)){
                 currentNumber = (currentNumber * 10) + (c - '0');
-                
             }
             
-            if((!Character.isDigit(c) && !Character.isWhitespace(c))|| i == s.length()-1){
-                    switch(operation){
-                        case '-':
-                            stack.push(-currentNumber);
-                            break;
-                        case '+':
-                            stack.push(currentNumber);
-                            break;
-                        case '/':
-                            stack.push(stack.pop()/currentNumber);
-                            break;
-                        case '*':
-                            stack.push(stack.pop()*currentNumber);
-                            break;
-                    }
-            operation = c;
-            currentNumber = 0;
+            if(!Character.isDigit(c) && !Character.isWhitespace(c) || i == s.length()-1){
+                switch(operation){
+                    case '+':
+                        stack.push(currentNumber);
+                        break;
+                    case '-':
+                        stack.push(-currentNumber);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * currentNumber);
+                        break;
+                    case '/':
+                        stack.push(stack.pop() / currentNumber);
+                        break;
+                }
+                currentNumber = 0;
+                operation = c;
             }
-                
-          }
-        
-        
-        int result = 0;
-        while(!stack.isEmpty()){
-            result += stack.pop();
         }
         
-        return result;
+        int ans = 0;
+        while(!stack.isEmpty()){
+            ans += stack.pop();
+        }
+        
+        return ans;
     }
 }
