6e5b4ba4dab3b57369edfa2b38dcf54f135d2496
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2024-12-11 15:56:26 -0800

    Time: 5 ms (92.90%) | Memory: 60.8 MB (20.90%) - LeetSync

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        if(nums.length == 1){
+            return 1;
+        }
+        
+        int maxValue = 0;
+        
+        for(int num : nums){
+            maxValue = Math.max(maxValue, num);
+        }
+        
+        int[] tmp = new int[maxValue+1];
+        
+        for(int num : nums){
+            tmp[Math.max(0,num-k)]++;
+            tmp[Math.min(maxValue, num+k+1)]--;
+        }
+        
+        int maxSeq = 0;
+        int runningSum = 0;
+        for(int val : tmp){
+            runningSum += val;
+            maxSeq = Math.max(maxSeq, runningSum);
+        }
+        
+        return maxSeq;
+    }
+}
