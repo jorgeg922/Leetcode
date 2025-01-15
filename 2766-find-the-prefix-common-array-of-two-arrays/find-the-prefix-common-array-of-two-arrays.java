f08294f7d7bce006557efd412ad1695e46ca0835
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2025-01-14 16:35:40 -0800

    Time: 5 ms (61.84%) | Memory: 46.1 MB (7.37%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -3,25 +3,25 @@ class Solution {
         int n = A.length;
         int[] ans = new int[n];
 
-        Map<Integer,Integer> seenA = new HashMap<>();
-        Map<Integer, Integer> seenB = new HashMap<>();
+        Set<Integer> seenA = new HashSet<>();
+        Set<Integer> seenB = new HashSet<>();
 
         int prevCount = 0;
         for(int i=0; i<n; i++){
             int A_val = A[i];
             int B_val = B[i];
 
-            seenA.put(A_val, 1);
-            seenB.put(B_val, 1);
+            seenA.add(A_val);
+            seenB.add(B_val);
 
             int newCount = 0;
             if(A_val == B_val){
                 newCount++;
             }else{
-                if(seenB.containsKey(A_val)){
+                if(seenB.contains(A_val)){
                     newCount++;
                 }
-                if(seenA.containsKey(B_val)){
+                if(seenA.contains(B_val)){
                     newCount++;
                 }
             }
