bdbfec3ae38290356759e70d0cb7bc34ebc7adb4
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2025-03-28 01:20:20 -0700

    Time: 8 ms (9.78%) | Memory: 43.9 MB (75.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String number = Integer.toString(x);
-
-        int left = 0;
-        int right = number.length()-1;
-
-        while(left < right){
-            if(number.charAt(left) != number.charAt(right)){
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
+        StringBuilder sb = new StringBuilder(number);
+        return sb.reverse().toString().equals(number);
     }
 }
