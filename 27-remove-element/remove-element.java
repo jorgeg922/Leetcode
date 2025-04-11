3addfcc98795a195c8d3fe30971c41c75f5bd05c
Jorge Gonzales <23348580+jorgeg922@users.noreply.github.com>
2025-04-11 12:57:03 -0700

    Time: 1 ms (5.36%) | Memory: 42 MB (67.25%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,24 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-       int left = 0;
-        int right = nums.length-1;
         
-        while(left <= right){
-            if(nums[left] == val){
-                swap(left, right, nums);
-                right--;
-                continue;
+
+        int counter = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == val){
+                nums[i] = Integer.MAX_VALUE;
+                counter++;
             }
-            
-            left++;
         }
-        
-        return left;
-    }
-    
-    public void swap(int a, int b, int[] nums){
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
+
+        Arrays.sort(nums);
+        return nums.length - counter;  
     }
 }
